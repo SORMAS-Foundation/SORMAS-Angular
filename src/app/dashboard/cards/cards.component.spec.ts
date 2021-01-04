2b48a4990b77c9203901f63dabb597a7d2d86752
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-04 17:59:52 +0200

    fix tests

--- src/app/dashboard/cards/cards.component.spec.ts
+++ src/app/dashboard/cards/cards.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CardsComponent } from './cards.component';
 
@@ -8,6 +9,7 @@ describe('CardsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [CardsComponent],
     }).compileComponents();
   });
--- src/app/services/disease.service.spec.ts
+++ src/app/services/disease.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DiseaseService } from './disease.service';
 
@@ -6,7 +7,9 @@ describe('DiseaseServiceService', () => {
   let service: DiseaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DiseaseService);
   });
 
