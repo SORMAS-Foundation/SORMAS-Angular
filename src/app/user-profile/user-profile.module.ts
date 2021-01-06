3b623924b9264c97ddb1ca5fffca13d918258ab7
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-06 17:31:00 +0200

    add a simple locale switch

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [UserProfileComponent],
-  imports: [CommonModule, UserProfileRoutingModule],
+  imports: [CommonModule, UserProfileRoutingModule, SharedModule],
 })
 export class UserProfileModule {}
