af2d9338ed4f91e4748b76ceb42d9e6f51ac18a9
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-08 12:24:50 +0200

    remove PDS styles

--- src/app/pds-playground/pds-playground.component.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pds-playground',
-  templateUrl: './pds-playground.component.html',
-  styleUrls: ['./pds-playground.component.scss'],
-})
-export class PdsPlaygroundComponent {}
