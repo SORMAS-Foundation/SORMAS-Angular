f231da586e12ffad48464d9f0564f9634cc4860b
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-08 17:07:20 +0200

    add material module & locale switch use material comp

---
+++ src/app/shared/locale-select/localeOptions.ts
@@ -0,0 +1,14 @@
+export const localeOptions = [
+  {
+    locale: 'en-US',
+    lang: 'English',
+  },
+  {
+    locale: 'fr',
+    lang: 'French',
+  },
+  {
+    locale: 'de',
+    lang: 'German',
+  },
+];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LocaleSelectComponent } from './locale-select/locale-select.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [LayoutComponent, NotFoundComponent, LocaleSelectComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [LayoutComponent, NotFoundComponent, LocaleSelectComponent],
+  imports: [CommonModule, RouterModule, MaterialModule],
+  exports: [LayoutComponent, NotFoundComponent, LocaleSelectComponent, MaterialModule],
 })
 export class SharedModule {}
