cb26dca5dc01b18270d559a31443b72d0b772ce5
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-11 18:29:15 +0200

    improve tests

--- src/app/user-profile/user-profile.component.spec.ts
+++ src/app/user-profile/user-profile.component.spec.ts
@@ -1,9 +1,14 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipHarness } from '@angular/material/chips/testing';
 
 import { KeycloakService } from 'keycloak-angular';
+import { SharedModule } from '../shared/shared.module';
 import { UserProfileComponent } from './user-profile.component';
 
-const mockRoles = ['admin', 'role-A'];
+const mockRoles = ['admin', 'role-A', 'role-B'];
 
 class MockKeycloakService extends KeycloakService {
   getUsername(): string {
@@ -14,36 +19,40 @@ class MockKeycloakService extends KeycloakService {
   }
 }
 
+let loader: HarnessLoader;
+
 describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [SharedModule, NoopAnimationsModule],
       declarations: [UserProfileComponent],
       providers: [{ provide: KeycloakService, useValue: new MockKeycloakService() }],
     }).compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(UserProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render user name', () => {
-    const compiled = fixture.nativeElement;
+  it('should render header and card', () => {
+    const card = document.getElementsByTagName('mat-card');
+    const header = document.getElementsByTagName('h2');
 
-    expect(compiled.querySelector('p[data-test-id="user-name"]').textContent).toBe(
-      ' User name: test-user'
-    );
+    expect(card[0]).toBeDefined();
+    expect(header[0].textContent).toBe('User profile');
+  });
 
-    mockRoles.forEach((role) => {
-      expect(compiled.innerHTML).toContain(role);
-    });
+  it('renderes roles', async () => {
+    const chips = await loader.getAllHarnesses(MatChipHarness);
+    const rolesInChips = await Promise.all(chips.map((c) => c.getText()));
+
+    expect(rolesInChips).toEqual(mockRoles);
   });
 });
