089cfbda62d888c613078d96da3d3e66d0bb5eba
Sandor <sandor.ban@clouduniserv.onmicrosoft.com>
2021-01-19 15:05:40 +0200

    Extract some constants

--- src/app/services/form-element-control.service.ts
+++ src/app/services/form-element-control.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { FormElementBase } from '../shared/dynamic-form/types/form-element-base';
 
+const MAX_AGE = 100;
+const MIN_PASS_LENGTH = 8;
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,7 +12,6 @@ export class FormElementControlService {
   toFormGroup = (formElements: FormElementBase<string>[]) => {
     const group: { [key: string]: FormControl } = {};
     let validations: ValidatorFn[] = [];
-    // TODO : Extract the validation logic in separate ts
     formElements.forEach((formElement) => {
       if (formElement.validation?.includes('required')) {
         validations.push(Validators.required);
@@ -18,10 +20,10 @@ export class FormElementControlService {
         validations.push(Validators.email);
       }
       if (formElement.validation?.includes('age')) {
-        validations.push(Validators.max(100));
+        validations.push(Validators.max(MAX_AGE));
       }
       if (formElement.validation?.includes('password')) {
-        validations.push(Validators.minLength(8));
+        validations.push(Validators.minLength(MIN_PASS_LENGTH));
       }
       group[formElement.key] =
         validations.length > 0
