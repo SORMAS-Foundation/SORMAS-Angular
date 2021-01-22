3a10a025ebde80fa4c40d0ab60893e7c4777acf7
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-22 13:22:28 +0200

    fix tests

--- src/app/dashboard/cards/cards.component.spec.ts
+++ src/app/dashboard/cards/cards.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiModule } from 'api-client';
 
 import { CardsComponent } from './cards.component';
 
@@ -9,7 +10,7 @@ describe('CardsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, ApiModule],
       declarations: [CardsComponent],
     }).compileComponents();
   });
