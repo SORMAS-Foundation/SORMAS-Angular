bd9c0a2fb0bca1d25481ec514fbd1cf7d6b798a6
daniel.rus <daniel.rus@freshbyteinc.com>
2021-02-23 17:14:45 +0200

    cleanup

--- src/app/shared/layout/menu-items/menu-items.component.ts
+++ src/app/shared/layout/menu-items/menu-items.component.ts
@@ -1,27 +1,17 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Injectable,
-  Input,
-  Output,
-} from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
+import { Component, EventEmitter, Injectable, Input, Output } from '@angular/core';
 import { RouteItem } from '../shared/route-item.model';
 
 @Component({
   selector: 'app-menu-items',
   templateUrl: './menu-items.component.html',
   styleUrls: ['./menu-items.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [MatDrawer],
 })
 @Injectable()
 export class MenuItemsComponent {
   @Input() routeConfig: RouteItem[] = [];
   @Output() routeChange: EventEmitter<any> = new EventEmitter();
 
-  doRouteChange(route: RouteItem): void {
+  announceRouteChange(route: RouteItem): void {
     this.routeChange.emit(route);
   }
 }
