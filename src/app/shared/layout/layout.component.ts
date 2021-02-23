00eaa1b3dc4d306a67148e57fda05397cf3c38f5
daniel.rus <daniel.rus@freshbyteinc.com>
2021-02-23 19:46:24 +0200

    add media service

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,14 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import logoPath from '../../../assets/img/sormas-logo.png';
+import { CustomBreakpointNames } from '../../services/media/breakpoints.service';
+import { MediaService } from '../../services/media/media.service';
 import { routesConfig } from './shared/routesConfig';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   logo = logoPath;
   navigation = routesConfig;
+  showMenuHeader = true;
+  showMenuDrawer = true;
+  showButtonBurger = true;
+
+  constructor(private mediaService: MediaService) {}
+
+  ngOnInit(): void {
+    this.mediaService.subscribeToLayoutChanges().subscribe(() => {
+      const IS_MEDIUM = this.mediaService.isBreakpointActive(CustomBreakpointNames.medium);
+      this.showMenuHeader = !IS_MEDIUM;
+      this.showMenuDrawer = !!IS_MEDIUM;
+      this.showButtonBurger = !!IS_MEDIUM;
+    });
+  }
 }
