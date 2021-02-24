c53ee436d6a0e717afa59e7559d7f6eaedbcf480
Sandor Ban <zorckmaster@gmail.com>
2021-02-24 13:41:39 +0200

    Fix mobile version

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { MatIconRegistry } from '@angular/material/icon';
 import logoPath from '../../../assets/img/sormas-logo.svg';
 import { CustomBreakpointNames } from '../../services/media/breakpoints.service';
 import { MediaService } from '../../services/media/media.service';
 import { routesConfig } from './shared/routesConfig';
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  @ViewChild('sidenav')
-  sidenav!: MatSidenav;
+export class LayoutComponent implements AfterContentInit {
+  @ViewChild('sidenav', { static: true }) sidenav!: MatSidenav;
   logo = logoPath;
   navigation = routesConfig;
   showMenuHeader = true;
   showButtonBurger = true;
-
-  constructor(private mediaService: MediaService, private matIconRegistry: MatIconRegistry) {}
-
-  ngOnInit(): void {
+  constructor(private mediaService: MediaService) {}
+  ngAfterContentInit(): void {
     this.mediaService.subscribeToLayoutChanges().subscribe(() => {
       const IS_MEDIUM = this.mediaService.isBreakpointActive(CustomBreakpointNames.medium);
       this.showMenuHeader = !IS_MEDIUM;
