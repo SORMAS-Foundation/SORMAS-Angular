12ddacc8badc156af51f6dd0724a1ac2e13dcfd2
Sandor Ban <zorckmaster@gmail.com>
2021-02-24 17:03:55 +0200

    Fix other failing test

--- src/app/shared/layout/layout.component.spec.ts
+++ src/app/shared/layout/layout.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../shared.module';
 import { LayoutComponent } from './layout.component';
 
 describe('LayoutComponent', () => {
@@ -9,6 +12,7 @@ describe('LayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LayoutComponent],
+      imports: [SharedModule, BrowserAnimationsModule, RouterTestingModule],
     }).compileComponents();
   });
 
