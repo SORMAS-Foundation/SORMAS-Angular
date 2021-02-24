46d51cdeb04781341eff9d54237c2e5015f70e35
Sandor Ban <zorckmaster@gmail.com>
2021-02-24 12:44:28 +0200

    Fixes

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import logoPath from '../../../assets/img/sormas-logo.png';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatIconRegistry } from '@angular/material/icon';
+import logoPath from '../../../assets/img/sormas-logo.svg';
 import { CustomBreakpointNames } from '../../services/media/breakpoints.service';
 import { MediaService } from '../../services/media/media.service';
 import { routesConfig } from './shared/routesConfig';
@@ -10,18 +12,23 @@ import { routesConfig } from './shared/routesConfig';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  @ViewChild('sidenav')
+  sidenav!: MatSidenav;
   logo = logoPath;
   navigation = routesConfig;
   showMenuHeader = true;
   showButtonBurger = true;
 
-  constructor(private mediaService: MediaService) {}
+  constructor(private mediaService: MediaService, private matIconRegistry: MatIconRegistry) {}
 
   ngOnInit(): void {
     this.mediaService.subscribeToLayoutChanges().subscribe(() => {
       const IS_MEDIUM = this.mediaService.isBreakpointActive(CustomBreakpointNames.medium);
       this.showMenuHeader = !IS_MEDIUM;
       this.showButtonBurger = !!IS_MEDIUM;
+      if (IS_MEDIUM) {
+        this.sidenav.close();
+      }
     });
   }
 }
