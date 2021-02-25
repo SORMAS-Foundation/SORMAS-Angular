b43502a4c2d37e452dae32a617094e21ef1f8e54
Sandor Ban <zorckmaster@gmail.com>
2021-02-25 15:09:16 +0200

    basic tabs

--- src/app/case/case.component.ts
+++ src/app/case/case.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CaseControllerService, CaseDataDto } from 'api-client';
 
 @Component({
   selector: 'app-case',
   templateUrl: './case.component.html',
   styleUrls: ['./case.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CaseComponent implements OnInit {
   case: CaseDataDto | undefined;
--- src/app/case/case.module.ts
+++ src/app/case/case.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from '../material.module';
 import { CaseRoutingModule } from './case-routing.module';
 import { CaseComponent } from './case.component';
 
 @NgModule({
   declarations: [CaseComponent],
-  imports: [CommonModule, CaseRoutingModule],
+  imports: [CommonModule, CaseRoutingModule, MaterialModule],
 })
 export class CaseModule {}
