eb5f3f80b36b28a891ae2d2e9526aa00e2db6927
Sandor Ban <zorckmaster@gmail.com>
2021-03-02 17:50:12 +0200

    Cases-header info-box & styling

--- src/app/case/case.component.ts
+++ src/app/case/case.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CaseControllerService, CaseDataDto } from 'api-client';
+import { CaseClassificationIcons, CaseOutcomeIcons } from './icons-assignment';
 
 @Component({
   selector: 'app-case',
   templateUrl: './case.component.html',
   styleUrls: ['./case.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class CaseComponent implements OnInit {
   case: CaseDataDto | undefined;
-  caseId = 'R674FI-J5G2Z7-GOEVQR-WAQCCKGM'; // todo - get from Route
-
+  caseId = 'W5GUPC-LBYRTF-XM2B6S-VEZXSJJU'; // todo - get from Route when verview page is ready
+  caseOutcomeIcons = CaseOutcomeIcons;
+  caseClassificationIcons = CaseClassificationIcons;
   constructor(private caseService: CaseControllerService) {}
 
   async ngOnInit(): Promise<void> {
     const [caseData] = await this.caseService.getByUuids6([this.caseId]).toPromise();
     this.case = caseData;
-
-    // this.case!.person.lastName
+    this.caseOutcomeIcons = CaseOutcomeIcons;
+    this.caseClassificationIcons = CaseClassificationIcons;
   }
 }
