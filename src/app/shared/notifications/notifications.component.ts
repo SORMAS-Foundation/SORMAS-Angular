370df6ca3ba047122d42b3e7c5ae94fdb10042f2
daniel.rus <daniel.rus@freshbyteinc.com>
2021-03-05 09:57:50 +0200

    create basic module

---
+++ src/app/shared/notifications/notifications.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.scss'],
+})
+export class NotificationsComponent {}
---
+++ src/app/shared/notifications/notifications.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotificationsComponent } from './notifications.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [NotificationsComponent],
+})
+export class NotificationsModule {}
