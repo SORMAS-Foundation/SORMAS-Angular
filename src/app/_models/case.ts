5f868b81d8063def6fbade298eb4144cf942b396
Sandor Ban <zorckmaster@gmail.com>
2021-03-17 11:30:32 +0200

    Regroup models

--- src/app/_models/case.ts
+++ src/app/_models/case.ts
@@ -1,3 +1,7 @@
+import { Disease } from '../../../dist/api-client/public-api';
+import { Person } from './person';
+import { User } from './user';
+
 export type CaseOutcome = 'NO_OUTCOME' | 'DECEASED' | 'RECOVERED' | 'UNKNOWN';
 export type CaseClassification =
   | 'NOT_CLASSIFIED'
@@ -7,76 +11,6 @@ export type CaseClassification =
   | 'CONFIRMED_NO_SYMPTOMS'
   | 'CONFIRMED_UNKNOWN_SYMPTOMS'
   | 'NO_CASE';
-
-export interface Person {
-  uuid?: string;
-  caption?: string;
-  firstName?: string;
-  lastName?: string;
-}
-
-export interface User {
-  uuid?: string;
-  caption?: string;
-}
-
-export type Disease =
-  | 'AFP'
-  | 'CHOLERA'
-  | 'CONGENITAL_RUBELLA'
-  | 'CSM'
-  | 'DENGUE'
-  | 'EVD'
-  | 'GUINEA_WORM'
-  | 'LASSA'
-  | 'MEASLES'
-  | 'MONKEYPOX'
-  | 'NEW_INFLUENZA'
-  | 'PLAGUE'
-  | 'POLIO'
-  | 'UNSPECIFIED_VHF'
-  | 'WEST_NILE_FEVER'
-  | 'YELLOW_FEVER'
-  | 'RABIES'
-  | 'ANTHRAX'
-  | 'CORONAVIRUS'
-  | 'PNEUMONIA'
-  | 'MALARIA'
-  | 'TYPHOID_FEVER'
-  | 'ACUTE_VIRAL_HEPATITIS'
-  | 'NON_NEONATAL_TETANUS'
-  | 'HIV'
-  | 'SCHISTOSOMIASIS'
-  | 'SOIL_TRANSMITTED_HELMINTHS'
-  | 'TRYPANOSOMIASIS'
-  | 'DIARRHEA_DEHYDRATION'
-  | 'DIARRHEA_BLOOD'
-  | 'SNAKE_BITE'
-  | 'RUBELLA'
-  | 'TUBERCULOSIS'
-  | 'LEPROSY'
-  | 'LYMPHATIC_FILARIASIS'
-  | 'BURULI_ULCER'
-  | 'PERTUSSIS'
-  | 'NEONATAL_TETANUS'
-  | 'ONCHOCERCIASIS'
-  | 'DIPHTERIA'
-  | 'TRACHOMA'
-  | 'YAWS_ENDEMIC_SYPHILIS'
-  | 'MATERNAL_DEATHS'
-  | 'PERINATAL_DEATHS'
-  | 'INFLUENZA_A'
-  | 'INFLUENZA_B'
-  | 'H_METAPNEUMOVIRUS'
-  | 'RESPIRATORY_SYNCYTIAL_VIRUS'
-  | 'PARAINFLUENZA_1_4'
-  | 'ADENOVIRUS'
-  | 'RHINOVIRUS'
-  | 'ENTEROVIRUS'
-  | 'M_PNEUMONIAE'
-  | 'C_PNEUMONIAE'
-  | 'OTHER'
-  | 'UNDEFINED';
 
 export interface CaseItem {
   id: number | string;
---
+++ src/app/_models/user.ts
@@ -0,0 +1,4 @@
+export interface User {
+  uuid?: string;
+  caption?: string;
+}
