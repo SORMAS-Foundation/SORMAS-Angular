07b27b95ce741a8a57fcf3288e4a87e4078722b4
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-03-19 11:22:28 +0200

    lint fix

--- src/app/cases/case_components/case-details/form-data.ts
+++ src/app/cases/case_components/case-details/form-data.ts
@@ -1,55 +1,33 @@
 export const FORM_DATA_CASE_DETAILS = [
   {
-    "title": "Vaccine Name",
-    "fields": [
+    title: 'Vaccine Name',
+    fields: [
       {
-        "key": "vaccineName",
-        "label": "Vaccine Name",
-        "value": "",
-        "validation": ["required"],
-        "order": 1,
-        "controlType": "input",
-        "options": [],
-        "type": "text",
-        "newLine": false
+        key: 'vaccineName',
+        label: 'Vaccine Name',
+        value: '',
+        validation: ['required'],
+        order: 1,
+        controlType: 'input',
+        options: [],
+        type: 'text',
+        newLine: false
       }
     ]
   },
   {
-    "title": "Other stuff",
-    "fields": [
+    title: 'Other stuff',
+    fields: [
       {
-        "key": "disease",
-        "label": "Disease",
-        "value": "",
-        "validation": ["required"],
-        "order": 1,
-        "controlType": "input",
-        "options": [],
-        "type": "text",
-        "newLine": false
-      },
-      {
-        "key": "caseClassification",
-        "label": "Case Classification",
-        "value": "",
-        "validation": ["required"],
-        "order": 1,
-        "controlType": "input",
-        "options": [],
-        "type": "text",
-        "newLine": false
-      },
-      {
-        "key": "additionalDetails",
-        "label": "Additional details",
-        "value": "",
-        "validation": ["required"],
-        "order": 1,
-        "controlType": "input",
-        "options": [],
-        "type": "text",
-        "newLine": true
+        key: 'disease',
+        label: 'Disease',
+        value: '',
+        validation: ['required'],
+        order: 1,
+        controlType: 'input',
+        options: [],
+        type: 'text',
+        newLine: false
       }
     ]
   }
