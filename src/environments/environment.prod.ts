80ee49a084b8ce6f00ff5395455616f31c35ef35
Horia Ardelean <horia.ardelean@freshbyeinc.com>
2021-03-23 15:02:55 +0200

    Updated docker-compose.yml and documentation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   keycloakUrl: 'https://sormas-docker-test.com/keycloak/auth/',
   keycloakRealm: 'SORMAS',
-  keycloakClientId: 'sormas-angular-prod',
+  keycloakClientId: 'sormas-angular',
   apiUrl: 'https://sormas-docker-test.com',
   isLegacyLogin: false,
 };
