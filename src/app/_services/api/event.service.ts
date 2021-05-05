e6276d994e1e767d1c817b51db8783e9b9d1b35d
daniel.rus <daniel.rus@freshbyteinc.com>
2021-05-05 10:54:41 +0300

    update event service

--- src/app/_services/api/event.service.ts
+++ src/app/_services/api/event.service.ts
@@ -6,12 +6,13 @@ import { EventSerializer } from '../../_serializers/event.serializer';
 
 import * as constants from '../../app.constants';
 import { EventDto } from '../../_models/eventDto';
+import { HelperService } from '../helper.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventService extends BaseService<EventDto> {
-  constructor(httpClient: HttpClient) {
-    super(httpClient, '', constants.API_ROUTE_EVENTS, new EventSerializer());
+  constructor(httpClient: HttpClient, helperService: HelperService) {
+    super(httpClient, helperService, '', constants.API_ROUTE_EVENTS, new EventSerializer());
   }
 }
