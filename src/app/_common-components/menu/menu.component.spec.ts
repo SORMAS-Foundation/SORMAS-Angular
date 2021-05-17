e2ca94a270713e0f2f744c67794720db02fea6b2
Sandor Ban <zorckmaster@gmail.com>
2021-05-17 10:02:00 +0300

    Fix failing test

--- src/app/_common-components/menu/menu.component.spec.ts
+++ src/app/_common-components/menu/menu.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { MenuComponent } from './menu.component';
@@ -10,7 +11,7 @@ describe('MenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MenuComponent],
-      imports: [TranslateModule.forRoot()],
+      imports: [TranslateModule.forRoot(), BrowserAnimationsModule],
     }).compileComponents();
   });
 
