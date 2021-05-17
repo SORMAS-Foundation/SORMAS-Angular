4204b64901232555f1addea2a799fabf867a73a3
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-05-17 09:33:01 +0300

    task filters

--- src/app/tasks/task-filters/task-filters.component.ts
+++ src/app/tasks/task-filters/task-filters.component.ts
@@ -1,10 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FilterService } from '../../_services/filter.service';
+import { Filter } from '../../_models/common';
 
 @Component({
   selector: 'app-task-filters',
   templateUrl: './task-filters.component.html',
   styleUrls: ['./task-filters.component.scss'],
 })
-export class TaskFiltersComponent {
+export class TaskFiltersComponent implements OnInit {
   @Input() drawer: any = {};
+
+  filtersForm = new FormGroup({});
+  allFilters: Filter[] = [];
+
+  constructor(private filterService: FilterService) {}
+
+  ngOnInit(): void {
+    this.initFiltersForm();
+  }
+
+  initFiltersForm(): void {
+    this.filtersForm = new FormGroup({
+      status: new FormControl(),
+      task: new FormControl(),
+      taskContent: new FormControl(),
+    });
+  }
+
+  filtersToArray(): void {
+    const keys: string[] = Object.keys(this.filtersForm.value);
+    const values: string[] = Object.values(this.filtersForm.value);
+    this.allFilters = [];
+    values.forEach((e, i) => {
+      if (values[i] !== null) {
+        this.allFilters.push({ field: keys[i], value: values[i] });
+      }
+    });
+    this.filterService.setFilters(this.allFilters);
+  }
+
+  resetFilters(): void {
+    this.initFiltersForm();
+    this.filtersToArray();
+  }
+  onFormChange(): void {
+    this.filtersToArray();
+  }
 }
