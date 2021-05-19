e6b10898ef96a5837adba02f20f504ae95288f28
Daniel Rus <daniel.rus@freshbyteinc.com>
2021-05-19 14:33:45 +0300

    Port health form (#117)
    * add port health tab
    
    * add translations
    
    * remove subscription on component destroy

--- src/app/_entity-data/case.ts
+++ src/app/_entity-data/case.ts
@@ -8,6 +8,7 @@ export const caseLinks = (caseId: string): CaseLink[] => {
     { link: `/cases/case/${caseId}/details`, title: _('Case') },
     { link: `/cases/case/${caseId}/person`, title: _('Case person') },
     { link: `/cases/case/${caseId}/hospitalization`, title: _('Hospitalization') },
+    { link: `/cases/case/${caseId}/port-health`, title: _('Port health') },
     { link: `/cases/case/${caseId}/symptoms`, title: _('Symptoms') },
     { link: `/cases/case/${caseId}/epidemiological-data`, title: _('Epidemiological data') },
     { link: `/cases/case/${caseId}/therapy`, title: _('Therapy') },
