d781ee6baece3cb84a307b6a916544d085adcc1e
Sandor Ban <zorckmaster@gmail.com>
2021-05-27 15:56:34 +0300

    Fix failing test

--- src/app/persons/person-filters/person-filters.component.spec.ts
+++ src/app/persons/person-filters/person-filters.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { PersonFiltersComponent } from './person-filters.component';
 
@@ -9,6 +10,7 @@ describe('PersonFiltersComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PersonFiltersComponent],
+      imports: [TranslateModule.forRoot()],
     }).compileComponents();
   });
 
