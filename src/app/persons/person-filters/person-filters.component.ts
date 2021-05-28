191a169e203a60ec66597ede021f30b4bd8f1fd1
Daniel Rus <daniel.rus@freshbyteinc.com>
2021-05-28 18:43:56 +0300

    Filters for person list (#126)
    * Implement filters for persons list
    
    * Style adjustments for filters
    
    * Improve collapsable box

--- src/app/persons/person-filters/person-filters.component.ts
+++ src/app/persons/person-filters/person-filters.component.ts
@@ -1,10 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Filter } from '../../_models/common';
+import { FilterService } from '../../_services/filter.service';
 
 @Component({
   selector: 'app-person-filters',
   templateUrl: './person-filters.component.html',
   styleUrls: ['./person-filters.component.scss'],
 })
-export class PersonFiltersComponent {
+export class PersonFiltersComponent implements OnInit {
   @Input() drawer: any = {};
+
+  filtersForm = new FormGroup({});
+  allFilters: Filter[] = [];
+
+  constructor(private filterService: FilterService) {}
+
+  ngOnInit(): void {
+    this.initFiltersForm();
+  }
+
+  initFiltersForm(): void {
+    this.filtersForm = new FormGroup({
+      search: new FormControl(),
+      persons: new FormControl(),
+      presentCondition: new FormControl(),
+      birthdateYYYY: new FormControl(),
+      birthdateMM: new FormControl(),
+      birthdateDD: new FormControl(),
+      region: new FormControl(),
+      district: new FormControl(),
+      community: new FormControl(),
+    });
+  }
+
+  filtersToArray(): void {
+    const keys: string[] = Object.keys(this.filtersForm.value);
+    const values: string[] = Object.values(this.filtersForm.value);
+    this.allFilters = [];
+    values.forEach((e, i) => {
+      if (values[i] !== null) {
+        this.allFilters.push({ field: keys[i], value: values[i] });
+      }
+    });
+    this.filterService.setFilters(this.allFilters);
+  }
+
+  resetFilters(): void {
+    this.initFiltersForm();
+    this.filtersToArray();
+  }
+  onFormChange(): void {
+    this.filtersToArray();
+  }
 }
