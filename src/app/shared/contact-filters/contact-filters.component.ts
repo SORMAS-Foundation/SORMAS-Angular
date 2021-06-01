ed35c918a8f2455a50b361dc7b19bef6b6ab25f5
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-06-01 20:40:29 +0300

    contact filters

--- src/app/shared/contact-filters/contact-filters.component.ts
+++ src/app/shared/contact-filters/contact-filters.component.ts
@@ -10,6 +10,7 @@ import { FilterService } from '../../_services/filter.service';
 })
 export class ContactFiltersComponent implements OnInit {
   @Input() drawer: any = {};
+  @Input() inTab: boolean = false;
 
   filtersForm = new FormGroup({});
   allFilters: Filter[] = [];
@@ -21,33 +22,43 @@ export class ContactFiltersComponent implements OnInit {
   }
 
   initFiltersForm(): void {
-    this.filtersForm = new FormGroup({
-      search: new FormControl(),
-      eventSearch: new FormControl(),
-      contacts: new FormControl(),
-      status: new FormControl(),
-      traveler: new FormControl(),
-      birthdateYYYY: new FormControl(),
-      birthdateMM: new FormControl(),
-      birthdateDD: new FormControl(),
-      contactClassification: new FormControl(),
-      disease: new FormControl(),
-      caseClassification: new FormControl(),
-      followUpStatus: new FormControl(),
-      followUpUntilDate: new FormControl(),
-      responsibleRegion: new FormControl(),
-      responsibleDistrict: new FormControl(),
-      responsibleCommunity: new FormControl(),
-      contactPerson: new FormControl(),
-      reportedBy: new FormControl(),
-      moreFilters: new FormControl(),
-      placeOfQuarantine: new FormControl(),
-      quarantineEndDate: new FormControl(),
-      dateFilter: new FormControl(),
-      referenceDate: new FormControl(),
-      startDate: new FormControl(),
-      dueDate: new FormControl(),
-    });
+    if (this.inTab) {
+      this.filtersForm = new FormGroup({
+        search: new FormControl(),
+        contactClassification: new FormControl(),
+        contactPerson: new FormControl(),
+        responsibleRegion: new FormControl(),
+        responsibleOfficer: new FormControl(),
+      });
+    } else {
+      this.filtersForm = new FormGroup({
+        search: new FormControl(),
+        eventSearch: new FormControl(),
+        contacts: new FormControl(),
+        status: new FormControl(),
+        traveler: new FormControl(),
+        birthdateYYYY: new FormControl(),
+        birthdateMM: new FormControl(),
+        birthdateDD: new FormControl(),
+        contactClassification: new FormControl(),
+        disease: new FormControl(),
+        caseClassification: new FormControl(),
+        followUpStatus: new FormControl(),
+        followUpUntilDate: new FormControl(),
+        responsibleRegion: new FormControl(),
+        responsibleDistrict: new FormControl(),
+        responsibleCommunity: new FormControl(),
+        contactPerson: new FormControl(),
+        reportedBy: new FormControl(),
+        moreFilters: new FormControl(),
+        placeOfQuarantine: new FormControl(),
+        quarantineEndDate: new FormControl(),
+        dateFilter: new FormControl(),
+        referenceDate: new FormControl(),
+        startDate: new FormControl(),
+        dueDate: new FormControl(),
+      });
+    }
   }
 
   filtersToArray(): void {
