d4315ed3d1d0dce7dde91fa68b7c2157f6ec1d31
Sandor Ban <zorckmaster@gmail.com>
2021-06-17 10:44:06 +0300

    Fix failing test

--- src/app/tasks/task-add/task-add.component.spec.ts
+++ src/app/tasks/task-add/task-add.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskAddComponent } from './task-add.component';
@@ -9,6 +10,7 @@ describe('TaskAddComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TaskAddComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
