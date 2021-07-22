2564e161995c6b0dba06980767b10de365fad86b
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-07-22 18:37:35 +0300

    ..

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -5,5 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss'],
 })
-export class EventsComponent {
-}
+export class EventsComponent {}
