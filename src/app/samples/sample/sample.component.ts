ba2f115d09f5e84fbe0e8424cf81c8f0fa595164
Daniel Rus <daniel.rus@freshbyteinc.com>
2021-08-24 10:36:34 +0300

    Sample profile page (#202)
    * sample profile page
    
    * define constant for info bar types
    
    * info bar

--- src/app/samples/sample/sample.component.ts
+++ src/app/samples/sample/sample.component.ts
@@ -1,36 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { NotificationService } from '../../_services/notification.service';
 import { FormBase } from '../../shared/dynamic-form/types/form-element-base';
 import { FORM_DATA_PERSON } from './sample-form-data';
 import { FormElementControlService } from '../../_services/form-element-control.service';
-import { CaseDataDto, ContactDto, EventParticipantDto, SampleDto } from '../../_models/models';
+import { SampleDto } from '../../_models/models';
 import { SampleService } from '../../_services/api/sample.service';
 import { CaseService } from '../../_services/api/case.service';
 import { ContactService } from '../../_services/api/contact.service';
 import { InfoBarType, InfoBarTypeOptions } from '../../_models/common';
+import { EventParticipantService } from '../../_services/api/event-participant.service';
+import { PersonService } from '../../_services/api/person.service';
 import { EventService } from '../../_services/api/event.service';
-import { BaseService } from '../../_services/api/base.service';
 
 @Component({
   selector: 'app-sample',
   templateUrl: './sample.component.html',
   styleUrls: ['./sample.component.scss'],
 })
-export class SampleComponent implements OnInit {
+export class SampleComponent implements OnInit, OnDestroy {
   sample: SampleDto;
   sampleId: string;
-  additionalData: CaseDataDto | ContactDto | EventParticipantDto;
+  additionalData: any;
   additionalDataType: InfoBarType;
+  additionalDataLink: string;
   infoBarOptions = InfoBarTypeOptions;
   myFormElements: FormBase<any>[] = [];
   formData = FORM_DATA_PERSON;
+  subscriptions: Subscription[] = [];
 
   constructor(
     public sampleService: SampleService,
     private caseService: CaseService,
     private contactService: ContactService,
+    private eventparticipantService: EventParticipantService,
     private eventService: EventService,
+    private personService: PersonService,
     private activeRoute: ActivatedRoute,
     private formElementControlService: FormElementControlService,
     private notificationService: NotificationService
@@ -39,17 +45,19 @@ export class SampleComponent implements OnInit {
   ngOnInit(): void {
     const routeParams = this.activeRoute.snapshot.params;
     this.sampleId = routeParams.sampleId;
-    this.sampleService.getById(this.sampleId).subscribe({
-      next: (response: any) => {
-        this.sample = response;
-        this.initForm();
-        this.requestAdditionalInfo();
-      },
-      error: (err: any) => {
-        this.notificationService.error(err);
-      },
-      complete: () => {},
-    });
+    this.subscriptions.push(
+      this.sampleService.getById(this.sampleId).subscribe({
+        next: (response: any) => {
+          this.sample = response;
+          this.initForm();
+          this.requestAdditionalInfo();
+        },
+        error: (err: any) => {
+          this.notificationService.error(err);
+        },
+        complete: () => {},
+      })
+    );
   }
 
   initForm(): void {
@@ -60,34 +68,91 @@ export class SampleComponent implements OnInit {
   }
 
   requestAdditionalInfo(): void {
-    let service: BaseService<any> | undefined;
-    let id = '';
-
     if (this.sample.associatedCase) {
       this.additionalDataType = this.infoBarOptions.SAMPLE_CASE;
-      service = this.caseService;
-      id = this.sample.associatedCase.uuid;
+      this.requestCaseData(this.sample.associatedCase.uuid);
     } else if (this.sample.associatedContact) {
       this.additionalDataType = this.infoBarOptions.SAMPLE_CONTACT;
-      service = this.contactService;
-      id = this.sample.associatedContact.uuid;
+      this.requestContactData(this.sample.associatedContact.uuid);
     } else if (this.sample.associatedEventParticipant) {
       this.additionalDataType = this.infoBarOptions.SAMPLE_EVENT_PARTICIPANT;
-      service = this.eventService;
-      id = this.sample.associatedEventParticipant.uuid;
+      this.requestEventParticipantData(this.sample.associatedEventParticipant.uuid);
     }
+  }
 
-    if (service && id) {
-      service.getById(id).subscribe({
+  requestCaseData(id: string): void {
+    this.subscriptions.push(
+      this.caseService.getById(id).subscribe({
         next: (response: any) => {
           this.additionalData = response;
+          this.additionalDataLink = this.getAdditionalDataUrl();
+          this.requestPersonData(response.person.uuid);
         },
         error: (err: any) => {
           this.notificationService.error(err);
         },
         complete: () => {},
-      });
-    }
+      })
+    );
+  }
+
+  requestPersonData(id: string): void {
+    this.subscriptions.push(
+      this.personService.getById(id).subscribe({
+        next: (response: any) => {
+          this.additionalData.person = response;
+        },
+        error: (err: any) => {
+          this.notificationService.error(err);
+        },
+        complete: () => {},
+      })
+    );
+  }
+
+  requestEventParticipantData(id: string): void {
+    this.subscriptions.push(
+      this.eventparticipantService.getById(id).subscribe({
+        next: (response: any) => {
+          this.additionalData = response;
+          this.additionalDataLink = this.getAdditionalDataUrl();
+          this.requestEventData(response.event.uuid);
+        },
+        error: (err: any) => {
+          this.notificationService.error(err);
+        },
+        complete: () => {},
+      })
+    );
+  }
+
+  requestEventData(id: string): void {
+    this.subscriptions.push(
+      this.eventService.getById(id).subscribe({
+        next: (response: any) => {
+          this.additionalData.event = response;
+        },
+        error: (err: any) => {
+          this.notificationService.error(err);
+        },
+        complete: () => {},
+      })
+    );
+  }
+
+  requestContactData(id: string): void {
+    this.subscriptions.push(
+      this.contactService.getById(id).subscribe({
+        next: (response: any) => {
+          this.additionalData = response;
+          this.additionalDataLink = this.getAdditionalDataUrl();
+        },
+        error: (err: any) => {
+          this.notificationService.error(err);
+        },
+        complete: () => {},
+      })
+    );
   }
 
   getAdditionalDataUrl(): string {
@@ -101,10 +166,14 @@ export class SampleComponent implements OnInit {
         url = `/contacts/contact/${this.additionalData.uuid}`;
         break;
       case this.infoBarOptions.SAMPLE_EVENT_PARTICIPANT:
-        url = `/events/event/${this.additionalData.uuid}/participants`;
+        url = `/events/event/${this.additionalData.event.uuid}/details`;
         break;
       default:
     }
     return url;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
