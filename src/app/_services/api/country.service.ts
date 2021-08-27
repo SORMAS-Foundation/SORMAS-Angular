7470268cd42675d3a8d855f31a833e0de2a6b317
daniel.rus <daniel.rus@freshbyteinc.com>
2021-08-27 13:38:02 +0300

    Configuration districts

--- src/app/_services/api/country.service.ts
+++ src/app/_services/api/country.service.ts
@@ -5,13 +5,13 @@ import { BaseService } from './base.service';
 import { CountrySerializer } from '../../_serializers/country.serializer';
 
 import * as constants from '../../app.constants';
-import { EventDto } from '../../_models/eventDto';
+import { CountryDto } from '../../_models/models';
 import { HelperService } from '../helper.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CountryService extends BaseService<EventDto> {
+export class CountryService extends BaseService<CountryDto> {
   constructor(httpClient: HttpClient, helperService: HelperService) {
     super(httpClient, helperService, '', constants.API_ROUTE_COUNTRIES, new CountrySerializer());
   }
