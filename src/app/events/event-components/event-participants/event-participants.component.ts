7b8f99a92006b5f75c3e09592184fbc35d187f24
Sandor Ban <zorckmaster@gmail.com>
2021-08-31 12:12:44 +0300

    Fix push subscription

--- src/app/events/event-components/event-participants/event-participants.component.ts
+++ src/app/events/event-components/event-participants/event-participants.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Filter, NavItem, TableColumn } from '../../../_models/common';
 import { CONFIG_CASES } from '../../../_constants/storage';
 import { defaultColumnDefs } from './event-participants-list-table-data';
@@ -11,11 +12,14 @@ import { actionsBulkEditDefs } from './event-participants-list-actions-data';
   templateUrl: './event-participants.component.html',
   styleUrls: ['./event-participants.component.scss'],
 })
-export class EventParticipantsComponent implements OnInit {
+export class EventParticipantsComponent implements OnInit, OnDestroy {
   defaultColumns: TableColumn[] = [];
   configKey = CONFIG_CASES;
   actionsBulkEdit: NavItem[] = actionsBulkEditDefs;
   presetFilters: Filter[] = [];
+
+  private subscription: Subscription[] = [];
+
   constructor(
     public eventParticipantService: EventParticipantService,
     private activatedRoute: ActivatedRoute
@@ -23,8 +27,14 @@ export class EventParticipantsComponent implements OnInit {
 
   ngOnInit(): void {
     this.defaultColumns = defaultColumnDefs;
-    this.activatedRoute.params.subscribe((params) => {
-      this.presetFilters = [{ field: 'event', value: { uuid: params.eventId } }];
-    });
+    this.subscription.push(
+      this.activatedRoute.params.subscribe((params) => {
+        this.presetFilters = [{ field: 'event', value: { uuid: params.eventId } }];
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach((subscription) => subscription.unsubscribe());
   }
 }
