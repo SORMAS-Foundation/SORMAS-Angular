9394f40fab8f1c07b01e6f5909eac02ca0def51a
Sandor Ban <zorckmaster@gmail.com>
2021-09-20 13:53:44 +0300

    Fix failing tests

--- src/app/configuration/communities/community-add-edit/community-add-edit.component.spec.ts
+++ src/app/configuration/communities/community-add-edit/community-add-edit.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommunityAddEditComponent } from './community-add-edit.component';
@@ -9,6 +10,7 @@ describe('CommunityAddEditComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CommunityAddEditComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
