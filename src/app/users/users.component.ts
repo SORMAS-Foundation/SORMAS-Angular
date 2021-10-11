f7e12acbe90d06af48fe8435dd41b5b19526f165
daniel.rus <daniel.rus@freshbyteinc.com>
2021-10-11 11:51:24 +0300

    after merge fixes

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { HEADER_HEIGHT } from '../app.constants';
-import { FormBase } from '../shared/dynamic-form/types/form-element-base';
-import { FORM_DATA_USER_FILTERS } from './user-filters/user-filters-form-data';
 
 @Component({
   selector: 'app-users',
@@ -9,6 +7,5 @@ import { FORM_DATA_USER_FILTERS } from './user-filters/user-filters-form-data';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent {
-  filtersData: FormBase<any>[] = JSON.parse(JSON.stringify(FORM_DATA_USER_FILTERS));
   headerHeight = HEADER_HEIGHT;
 }
