4dd705c18f80bcb13ba9677771148e15529bf5b3
Sandor Ban <zorckmaster@gmail.com>
2021-10-28 09:47:10 +0300

    basic logic

--- src/app/contacts/contact-components/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-components/contact-details/contact-details.component.ts
@@ -9,6 +9,24 @@ import { TaskService } from '../../../_services/api/task.service';
 import { SampleService } from '../../../_services/api/sample.service';
 import { EventService } from '../../../_services/api/event.service';
 
+const contactRisks = [
+  { category: 'HIGH_RISK', values: ['TOUCHED_FLUID', 'FACE_TO_FACE_LONG', 'AEROSOL'] },
+  { category: 'HIGH_RISK_MED', values: ['MEDICAL_UNSAFE'] },
+  { category: 'MEDIUM_RISK_MED', values: ['MEDICAL_LIMITED'] },
+  { category: 'LOW_RISK', values: ['SAME_ROOM', 'FACE_TO_FACE_SHORT', 'MEDICAL_SAME_ROOM'] },
+  {
+    category: 'NO_RISK',
+    values: [
+      'MEDICAL_SAFE',
+      'MEDICAL_DISTANT',
+      'PHYSICAL_CONTACT',
+      'CLOTHES_OR_OTHER',
+      'CLOSE_CONTACT',
+      'AIRPLANE',
+    ],
+  },
+];
+
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
@@ -19,6 +37,7 @@ export class ContactDetailsComponent {
   formData = data.FORM_DATA_CONTACT_DETAILS;
   subscription: Subscription = new Subscription();
   contact: ContactDto;
+  selectedContactType = '';
   public resourceService: BaseService<any>;
 
   constructor(
@@ -43,5 +62,26 @@ export class ContactDetailsComponent {
     );
 
     this.contact = contactItem;
+  }
+
+  setRiskByContactType(contactType: string): void {
+    contactRisks.forEach((cr) => {
+      if (cr.values.includes(contactType)) {
+        this.myFormElements = this.formElementControlService.setAttributeToFormElement(
+          this.myFormElements,
+          'contactCategory',
+          'value',
+          cr.category
+        );
+        console.log('ok', cr.category, this.myFormElements);
+      }
+    });
+  }
+
+  onFormChange(event: any): void {
+    if (this.selectedContactType !== event.contactType) {
+      this.selectedContactType = event.contactType;
+      this.setRiskByContactType(this.selectedContactType);
+    }
   }
 }
