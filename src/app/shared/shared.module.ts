330716b0305e07a4be77989522c269d0ca768eed
daniel.rus <daniel.rus@freshbyteinc.com>
2021-11-02 20:01:51 +0200

    cleanup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -49,7 +49,6 @@ import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
 import { FiltersModule } from './filters/filters.module';
 import { AddressAddEditComponent } from './address-add-edit/address-add-edit.component';
 import { ContactAddEditComponent } from './contact-add-edit/contact-add-edit.component';
-import { DeleteModalComponent } from './modals/delete-modal/delete-modal.component';
 import { EntityActionsComponent } from './entity-actions/entity-actions.component';
 
 @NgModule({
@@ -86,7 +85,6 @@ import { EntityActionsComponent } from './entity-actions/entity-actions.componen
     CaseAddComponent,
     AddressAddEditComponent,
     ContactAddEditComponent,
-    DeleteModalComponent,
     EntityActionsComponent,
   ],
   imports: [
