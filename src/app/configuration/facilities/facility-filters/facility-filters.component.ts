cfe462b7dd7aa23371c95ee5032ecae6b572b94c
adriandumavitagroup <79842544+adriandumavitagroup@users.noreply.github.com>
2021-11-09 14:03:45 +0200

    facilities list (#262)

---
+++ src/app/configuration/facilities/facility-filters/facility-filters.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { FiltersFormComponent } from '../../../shared/filters/filters-form/filters-form.component';
+
+@Component({
+  selector: 'app-facility-filters',
+  templateUrl: '../../../shared/filters/filters-form/filters-form.component.html',
+})
+export class FacilityFiltersComponent extends FiltersFormComponent {}
