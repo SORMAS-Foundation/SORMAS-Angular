087f0d0c422671a0a9b051a4ee6cc1f0a9ac5c80
adriandumavitagroup <79842544+adriandumavitagroup@users.noreply.github.com>
2021-11-11 09:37:56 +0200

    Facility modal add edit (#267)
    * facilities modal add edit
    
    * facility add edit modal
    
    * fix

--- src/app/configuration/facilities/facility-list/facility-list.component.ts
+++ src/app/configuration/facilities/facility-list/facility-list.component.ts
@@ -1,30 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { TranslateService } from '@ngx-translate/core';
 import { NavItem, TableColumn } from '../../../_models/common';
 import { TableAppearanceOptions } from '../../../_constants/enums';
 import { FacilityService } from '../../../_services/api/facility.service';
 import { FacilityDto } from '../../../_models/facilityDto';
 import { defaultColumnDefs } from './facilities-table-data';
 import { actionsBulkEditDefs } from './facilities-actions-data';
+import { AddEditBaseModalComponent } from '../../../shared/modals/add-edit-base-modal/add-edit-base-modal.component';
+import { ADD_MODAL_MAX_WIDTH } from '../../../_constants/common';
+import { FacilityAddEditComponent } from '../facility-add-edit/facility-add-edit.component';
+import { FormActionsService } from '../../../_services/form-actions.service';
 
 @Component({
   selector: 'app-facility-list',
   templateUrl: './facility-list.component.html',
   styleUrls: ['./facility-list.component.scss'],
 })
-export class FacilityListComponent {
+export class FacilityListComponent implements OnDestroy {
   defaultColumns: TableColumn[] = defaultColumnDefs;
   actionsBulkEdit: NavItem[] = actionsBulkEditDefs;
   tableAppearanceOptions = TableAppearanceOptions;
 
-  constructor(public facilityService: FacilityService) {}
+  private subscription: Subscription[] = [];
+
+  constructor(
+    public facilityService: FacilityService,
+    private dialog: MatDialog,
+    private translateService: TranslateService,
+    private formActionsService: FormActionsService
+  ) {}
 
   openEditFacilityModal(facility: FacilityDto): void {
-    // eslint-disable-next-line no-console
-    console.log(facility);
+    const dialogRef = this.dialog.open(AddEditBaseModalComponent, {
+      maxWidth: ADD_MODAL_MAX_WIDTH,
+      data: {
+        title: this.translateService.instant('actionEditFacility'),
+        component: FacilityAddEditComponent,
+        resource: facility,
+        archive: true,
+      },
+    });
+
+    this.subscription.push(
+      dialogRef.afterClosed().subscribe((result) => {
+        this.formActionsService.setDiscard();
+        if (result) {
+          // this.tableComponent.getResources(true);
+        }
+      })
+    );
   }
 
   openAddFacilityModal(): void {
-    // eslint-disable-next-line no-console
-    console.log('add');
+    const dialogRef = this.dialog.open(AddEditBaseModalComponent, {
+      maxWidth: ADD_MODAL_MAX_WIDTH,
+      data: {
+        title: this.translateService.instant('actionAddNewFacility'),
+        component: FacilityAddEditComponent,
+      },
+    });
+
+    this.subscription.push(
+      dialogRef.afterClosed().subscribe((result) => {
+        this.formActionsService.setDiscard();
+        if (result) {
+          // this.tableComponent.getResources(true);
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach((subscription) => subscription.unsubscribe());
   }
 }
