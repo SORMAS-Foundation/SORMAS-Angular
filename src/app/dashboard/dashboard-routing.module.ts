75c5948f61a45a79fbce00fde029a57084e11a79
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-11-12 15:12:37 +0200

    dashboard base

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     component: DashboardComponent,
     children: [
       { path: '', component: SurveillanceComponent },
-      { path: 'contacts-dashboard', component: ContactsComponent },
+      { path: 'contact-dashboard', component: ContactsComponent },
     ],
   },
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router} from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NavItem } from '../_models/common';
+import { actionsViewOptionsDefs } from './dashboard-actions-data';
+import { ACTIONS_DASHBOARD_VIEW_OPTIONS } from '../_constants/actions';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnDestroy {
+  actionsViewOptions: NavItem[] = actionsViewOptionsDefs;
+  private subscription: Subscription[] = [];
+
+  public currentRoute: string;
+
+  constructor(private router: Router) {
+    this.subscription.push(
+      // @ts-ignore
+      this.router.events.subscribe((event: Event) => {
+        if (event instanceof NavigationEnd) {
+          if (event.url === '/') {
+            this.currentRoute = 'surveillance';
+          }
+          if (event.url.includes('contact-dashboard')) {
+            this.currentRoute = 'contacts';
+          }
+        }
+      })
+    );
+  }
+
+  onActionSelected(event: any): void {
+    switch (event) {
+      case ACTIONS_DASHBOARD_VIEW_OPTIONS.SURVEILLANCE:
+        this.router.navigate(['/']);
+        break;
+      case ACTIONS_DASHBOARD_VIEW_OPTIONS.CONTACTS:
+        this.router.navigate(['/contact-dashboard']);
+        break;
+      default:
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach((subscription) => subscription.unsubscribe());
+  }
+}
