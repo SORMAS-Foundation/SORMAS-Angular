8c4862fe4cf1004911603c83f8c5ff9939e8315c
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2021-11-12 15:58:11 +0200

    tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,6 +10,7 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
     }).compileComponents();
   });
 
