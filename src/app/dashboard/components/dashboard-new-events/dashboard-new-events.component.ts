d604e4c2c6e666f1512b1d0905567f2e3292876e
Sandor Ban <79192042+sandorbanvitagroup@users.noreply.github.com>
2021-12-24 10:02:00 +0200

    Feature/dashboard new events (#293)
    * Dashboard new events
    
    * Fix some translations
    
    * Added nwe tests & events & fixes
    
    * Fix tests
    
    * Add sum function
    
    * small fix
    
    Co-authored-by: Sandor Ban <zorckmaster@gmail.com>

--- src/app/dashboard/components/dashboard-new-events/dashboard-new-events.component.ts
+++ src/app/dashboard/components/dashboard-new-events/dashboard-new-events.component.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Filter } from '../../../_models/common';
+import { DashboardNewEventsService } from '../../../_services/api/dashboard-new-events-service';
+import { FilterService } from '../../../_services/filter.service';
+import { NotificationService } from '../../../_services/notification.service';
 
 @Component({
   selector: 'app-dashboard-new-events',
   templateUrl: './dashboard-new-events.component.html',
   styleUrls: ['./dashboard-new-events.component.scss'],
 })
-export class DashboardNewEventsComponent {}
+export class DashboardNewEventsComponent implements OnInit, OnDestroy {
+  newEvents: any;
+  subscriptions: Subscription[] = [];
+  filters: Filter[] = [];
+  sumEvents = 0;
+
+  constructor(
+    private dashboardNewEventsService: DashboardNewEventsService,
+    public filterService: FilterService,
+    private notificationService: NotificationService
+  ) {}
+
+  sumOfEvents(): number {
+    return (
+      this.newEvents.CLUSTER +
+        this.newEvents.EVENT +
+        this.newEvents.SIGNAL +
+        this.newEvents.SCREENING +
+        this.newEvents.DROPPED || 0
+    );
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.filterService.getFilters().subscribe((response) => {
+        this.filters = response.filters;
+        this.dashboardNewEventsService.getCalculated(this.filters).subscribe({
+          next: (data: any) => {
+            this.newEvents = data;
+            this.sumEvents = this.sumOfEvents();
+          },
+          error: (err: any) => {
+            this.notificationService.error(err);
+          },
+          complete: () => {},
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+}
