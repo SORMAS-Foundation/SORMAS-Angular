9a2fb5592026ff4f605435b04210c125440e3075
adriandumavitagroup <79842544+adriandumavitagroup@users.noreply.github.com>
2022-02-25 15:34:04 +0200

    Cases update + slideshow (#336)
    * facility fixes
    
    * ..
    
    * fixes
    
    * Revert "fixes"
    
    This reverts commit 00aa715408fd175bbe6dca8dc9422d8ab12a7012.
    
    * Revert "Revert "fixes""
    
    This reverts commit 22a5f5906e68a595d917482635e865e18e6f7c22.
    
    * cases detailed
    
    * fix
    
    * fix

--- src/app/shared/dynamic-form/components/form-lazy-options-base.component.ts
+++ src/app/shared/dynamic-form/components/form-lazy-options-base.component.ts
@@ -101,9 +101,13 @@ export class FormLazyOptionsBaseComponent extends FormBaseComponent implements O
     }
     const field = this.makeKey(key.replaceAll('.', '__'));
     const filters = Object.entries(this.makeObject(field, val))[0];
+
+    const keyMap = ['country', 'region', 'district', 'community'];
+    const filterName = keyMap.find((item) => key.toLowerCase().includes(item));
+
     return [
       {
-        field: filters[0],
+        field: filterName,
         value: filters[1],
       },
     ];
