a5573bfb106870e2ba1c39a1d814341e8e09311f
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2022-02-27 16:13:02 +0200

    fix

--- src/app/cases/cases-list/cases-list.component.ts
+++ src/app/cases/cases-list/cases-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
