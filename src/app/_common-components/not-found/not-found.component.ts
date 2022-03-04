06f8a5a9b31648baa13a4a76d437b93b77f5a06c
constantincrismaruvitagroup <constantin.crismaru.ext@vitagroup.ag>
2022-03-04 12:52:24 +0200

    added 404 not found page

--- src/app/_common-components/not-found/not-found.component.ts
+++ src/app/_common-components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -6,4 +7,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  constructor(private router: Router) { }
+  
+  navigateToDashboard() {
+    this.router.navigateByUrl('/');
+  }
+}
