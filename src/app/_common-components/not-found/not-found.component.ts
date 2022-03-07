66491bda16ff89b7c82be028f5469435327f6e22
constantincrismaruvitagroup <constantin.crismaru.ext@vitagroup.ag>
2022-03-07 15:55:16 +0200

    updated not-found-component router links

--- src/app/_common-components/not-found/not-found.component.ts
+++ src/app/_common-components/not-found/not-found.component.ts
@@ -5,12 +5,5 @@ import { Router } from '@angular/router';
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotFoundComponent {
-  constructor(private router: Router) { }
-  
-  navigateToDashboard() {
-    this.router.navigateByUrl('/');
-  }
-}
+export class NotFoundComponent { }
