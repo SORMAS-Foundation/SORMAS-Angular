ad6c0eac20ebc4cfd003cd5e22ceba240258e7dc
constantincrismaruvitagroup <constantin.crismaru.ext@vitagroup.ag>
2022-03-07 16:19:09 +0200

    fixed not-found-page style

--- src/app/_common-components/not-found/not-found.component.ts
+++ src/app/_common-components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { HelperService } from 'src/app/_services/helper.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent { }
+export class NotFoundComponent {
+  constructor(public helperService: HelperService) { }
+}
