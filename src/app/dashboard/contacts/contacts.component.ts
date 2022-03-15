6e2cd1cc231b9ac3c1018400acd34d4c35854e8b
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2022-03-15 16:46:31 +0200

    fix fulscreen

--- src/app/dashboard/contacts/contacts.component.ts
+++ src/app/dashboard/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DASHBOARD_EPIDEMIOLOGICAL_CURVE_TYPE } from '../../_constants/common';
+import { VIEW_OPTIONS, ViewOptions } from '../../_models/common';
 
 @Component({
   selector: 'app-contacts',
@@ -8,4 +9,9 @@ import { DASHBOARD_EPIDEMIOLOGICAL_CURVE_TYPE } from '../../_constants/common';
 })
 export class ContactsComponent {
   dashboardEpidemiologicalCurveType = DASHBOARD_EPIDEMIOLOGICAL_CURVE_TYPE;
+  fullscreenEpi = false;
+
+  onEpiCurveViewChange(event: ViewOptions): void {
+    this.fullscreenEpi = event === VIEW_OPTIONS.SECONDARY;
+  }
 }
