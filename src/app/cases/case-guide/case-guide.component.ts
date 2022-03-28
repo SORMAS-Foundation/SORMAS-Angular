fdc981650ea019b9c7758321761ef0f1a907d7f2
daniel.rus <daniel.rus@freshbyteinc.com>
2022-03-28 13:04:36 +0300

    Cleanup

--- src/app/cases/case-guide/case-guide.component.ts
+++ src/app/cases/case-guide/case-guide.component.ts
@@ -1,60 +1,8 @@
 import { Component } from '@angular/core';
-import { CardAppearanceOptions } from '../../_constants/enums';
 
 @Component({
   selector: 'app-case-guide',
   templateUrl: './case-guide.component.html',
   styleUrls: ['./case-guide.component.scss'],
 })
-export class CaseGuideComponent {
-  appearance = CardAppearanceOptions;
-  dataMyExports = [
-    {
-      title: 'Surveillance officer export for case data',
-      link: '/soefcd.pdf',
-    },
-    {
-      title: 'Anselm Adam export for person data',
-      link: '/aaefpd.pdf',
-    },
-    {
-      title: 'Export for general person',
-      link: '/efgp.pdf',
-    },
-    {
-      title: 'Export for general purpose',
-      link: '/efgp.pdf',
-    },
-    {
-      title: 'New export name',
-      link: '/nen.pdf',
-    },
-  ];
-
-  dataSharedExports = [
-    {
-      title: 'Surveillance officer export for case data',
-      link: '/soefcd.pdf',
-    },
-    {
-      title: 'Anselm Adam export for person data',
-      link: '/aaefpd.pdf',
-    },
-    {
-      title: 'Export for general person',
-      link: '/efgp.pdf',
-    },
-    {
-      title: 'Export for general purpose',
-      link: '/efgp.pdf',
-    },
-    {
-      title: 'New export name',
-      link: '/nen.pdf',
-    },
-    {
-      title: 'Shared export case management',
-      link: '/secm.pdf',
-    },
-  ];
-}
+export class CaseGuideComponent {}
