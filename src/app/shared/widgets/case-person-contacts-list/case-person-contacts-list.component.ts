2f790ef1adfa39d1b6c42c1f95bf336ba36396f4
Sandor Ban <zorckmaster@gmail.com>
2022-03-29 13:19:47 +0300

    delete some comments

--- src/app/shared/widgets/case-person-contacts-list/case-person-contacts-list.component.ts
+++ src/app/shared/widgets/case-person-contacts-list/case-person-contacts-list.component.ts
@@ -69,8 +69,6 @@ export class CasePersonContactsListComponent implements OnDestroy, OnInit {
   }
 
   deleteContact(contact: ContactDto): void {
-    console.log('contacts', this.contacts, contact);
-    // if()
     this.contacts = this.contacts.filter((item) => item !== contact);
     this.group.get(this.config.key)?.setValue(this.contacts);
     this.formActionsService.setInputChange(this.formId, this.config.key, true);
