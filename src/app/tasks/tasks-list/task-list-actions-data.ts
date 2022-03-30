60e05a5487f95ea07ce03ee33f7534171a2246cb
daniel.rus <daniel.rus@freshbyteinc.com>
2022-03-30 17:04:13 +0300

    UI fixes

--- src/app/tasks/tasks-list/task-list-actions-data.ts
+++ src/app/tasks/tasks-list/task-list-actions-data.ts
@@ -18,7 +18,7 @@ export const actionsBulkEditDefs: NavItem[] = [
   },
   {
     role: NavItemRole.ACTION,
-    name: 'captions.actionArchive',
+    name: 'captions.actionArchiveCoreEntity',
     action: ACTIONS_BULK_EDIT.ARCHIVE,
   },
 ];
