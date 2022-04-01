c6023659bb5511d40e5e7d784bcc246cf2ac7d76
Sandor Ban <zorckmaster@gmail.com>
2022-04-01 12:57:18 +0300

    Merge guide

---
+++ src/app/cases/merge-duplicates-case-guide/merge-duplicates-case-guide.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { MergeDuplicatesCaseGuideComponent } from './merge-duplicates-case-guide.component';
+
+describe('MergeDuplicatesCaseGuideComponent', () => {
+  let component: MergeDuplicatesCaseGuideComponent;
+  let fixture: ComponentFixture<MergeDuplicatesCaseGuideComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MergeDuplicatesCaseGuideComponent],
+      imports: [TranslateModule.forRoot()],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MergeDuplicatesCaseGuideComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
