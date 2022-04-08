1266508dcad10b16cc4456d5d587680abe91fea6
Sandor Ban <zorckmaster@gmail.com>
2022-04-08 11:24:16 +0300

    Don't extend baseService in exportService

--- src/app/_services/api/export.service.ts
+++ src/app/_services/api/export.service.ts
@@ -1,17 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Serializer } from '../../_serializers/serializer';
 import { HelperService } from '../helper.service';
-import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ExportService extends BaseService<any> {
-  constructor(httpClient: HttpClient, helperService: HelperService) {
-    super(httpClient, helperService, '', '', new Serializer());
-  }
+export class ExportService {
+  constructor(public helperService: HelperService, public httpClient: HttpClient) {}
   export(type: string, url: string): Observable<any> {
     return this.httpClient.post(`${this.helperService.getApiUrl()}/${url}`, {
       exportType: type,
