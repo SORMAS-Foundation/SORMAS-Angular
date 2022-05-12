ad7493b35dc2d0e8e33661da5ee716db46b60fce
adriandumavitagroup <79842544+adriandumavitagroup@users.noreply.github.com>
2022-05-12 14:40:24 +0300

    Fixes (#427)
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/app/merge-duplicates/merge-duplicates-table/merge-duplicates-table.component.ts
+++ src/app/merge-duplicates/merge-duplicates-table/merge-duplicates-table.component.ts
@@ -2,10 +2,13 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { DatepickerHeaderTodayComponent } from '../../shared/dynamic-form/components/datepicker-header-today/datepicker-header-today.component';
-import { DEFAULT_DATE_FORMAT } from '../../_constants/common';
+import { DEFAULT_DATE_FORMAT, MERGE_DUPLICATES_TYPE } from '../../_constants/common';
 import { MergeDuplicateDto } from '../../_models/mergeDuplicateDto';
 import { MergeDuplicateService } from '../../_services/api/mergeDuplicate.service';
 import { MergeDuplicateContactService } from '../../_services/api/mergeDuplicateContact.service';
+import { FilterService } from '../../_services/filter.service';
+import { MERGE_DUPLICATES_FILTERS_FORM_ID } from '../../_constants/form-identifiers';
+import { NotificationService } from '../../_services/notification.service';
 
 @Component({
   selector: 'app-merge-duplicates-table',
@@ -30,10 +33,17 @@ export class MergeDuplicatesTableComponent implements OnInit, OnDestroy {
 
   totalElementCount: number;
 
+  actionsDisabled: boolean = false;
+
+  hiddenUuids: any[] = [];
+  mergeDuplicatesType = MERGE_DUPLICATES_TYPE;
+
   constructor(
     private mergeDuplicatesService: MergeDuplicateService,
     private mergeDuplicatesContactService: MergeDuplicateContactService,
-    public translateService: TranslateService
+    public translateService: TranslateService,
+    private filterService: FilterService,
+    private notificationService: NotificationService
   ) {}
 
   ngOnInit(): void {
@@ -51,20 +61,39 @@ export class MergeDuplicatesTableComponent implements OnInit, OnDestroy {
       'creationDate',
       'completeness',
       'merge',
-      'hide',
       'pick',
+      'hide',
     ];
 
-    if (this.type === 'contacts') {
+    if (this.type === MERGE_DUPLICATES_TYPE.CONTACTS) {
       this.displayedColumns.splice(1, 0, 'caze');
     }
 
     this.getMergeDuplicates();
+
+    this.subscriptions.push(
+      this.filterService.getFilters().subscribe((response) => {
+        if (response.formId === MERGE_DUPLICATES_FILTERS_FORM_ID) {
+          if (!response.filters.length) {
+            this.hiddenUuids = [];
+          }
+
+          const filter = response.filters.find(
+            (item: { field: string }) => item.field === 'differingRegions'
+          );
+          if (filter) {
+            this.actionsDisabled = filter.value;
+          } else {
+            this.actionsDisabled = false;
+          }
+        }
+      })
+    );
   }
 
   getMergeDuplicates(concat: boolean = false): void {
     let service = this.mergeDuplicatesService;
-    if (this.type === 'contacts') {
+    if (this.type === MERGE_DUPLICATES_TYPE.CONTACTS) {
       service = this.mergeDuplicatesContactService;
     }
     this.subscriptions.push(
@@ -132,18 +161,52 @@ export class MergeDuplicatesTableComponent implements OnInit, OnDestroy {
   }
 
   mergeAction(element: any): void {
-    // eslint-disable-next-line no-console
-    console.log('element', element);
+    if (!this.actionsDisabled) {
+      this.notificationService
+        .prompt({
+          title: this.translateService.instant('strings.headingConfirmChoice'),
+          message: this.translateService.instant('strings.confirmationMergeCaseAndDeleteOther'),
+          buttonDeclineText: this.translateService.instant('captions.actionCancel'),
+          buttonConfirmText: this.translateService.instant('captions.actionConfirm'),
+        })
+        .subscribe((result) => {
+          if (result) {
+            if (result === 'CONFIRM') {
+              // eslint-disable-next-line no-console
+              console.log('merge action', element);
+            }
+          }
+        });
+    }
   }
 
   pickAction(element: any): void {
-    // eslint-disable-next-line no-console
-    console.log('element', element);
+    if (!this.actionsDisabled) {
+      this.notificationService
+        .prompt({
+          title: this.translateService.instant('strings.headingConfirmChoice'),
+          message: this.translateService.instant('strings.confirmationPickCaseAndDeleteOther'),
+          buttonDeclineText: this.translateService.instant('captions.actionCancel'),
+          buttonConfirmText: this.translateService.instant('captions.actionConfirm'),
+        })
+        .subscribe((result) => {
+          if (result) {
+            if (result === 'CONFIRM') {
+              // eslint-disable-next-line no-console
+              console.log('pick action', element);
+            }
+          }
+        });
+    }
   }
 
   hideAction(element: any): void {
-    // eslint-disable-next-line no-console
-    console.log('element', element);
+    this.hiddenUuids.push(this.mergeDuplicates[element].uuid);
+    this.hiddenUuids.push(this.mergeDuplicates[element + 1].uuid);
+  }
+
+  isHideAll(element: any): boolean {
+    return !!this.hiddenUuids.find((item) => item === element.uuid);
   }
 
   getTotal(): string {
