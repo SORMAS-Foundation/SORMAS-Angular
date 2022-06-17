6658d8db7e2fdfdfd89434c6b76ad5037cc09e37
Duma Adrian <adrian.duma.ext@vitagroup.ag>
2022-06-17 10:32:09 +0300

    fix

--- src/app/_services/api/helper-api.service.ts
+++ src/app/_services/api/helper-api.service.ts
@@ -3,16 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HelperService } from '../helper.service';
-import * as constants from "../../app.constants";
+import * as constants from '../../app.constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HelperApiService {
-  constructor(
-    protected httpClient: HttpClient,
-    protected helperService: HelperService,
-  ) {}
+  constructor(protected httpClient: HttpClient, protected helperService: HelperService) {}
 
   getLatLng(city: string, address: string): Observable<any> {
     const endpoint = constants.API_ROUTE_HELPER.GET_LAT_LNG;
--- src/app/shared/widgets/address-button/address-button.component.ts
+++ src/app/shared/widgets/address-button/address-button.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from "rxjs";
+import { Subscription } from 'rxjs';
 import { FormGroup } from '@angular/forms';
 import { HelperApiService } from '../../../_services/api/helper-api.service';
-import { FormElementBase } from "../../dynamic-form/types/form-element-base";
-import { NotificationService } from "../../../_services/notification.service";
+import { FormElementBase } from '../../dynamic-form/types/form-element-base';
+import { NotificationService } from '../../../_services/notification.service';
 
 @Component({
   selector: 'app-address-button',
@@ -22,7 +22,9 @@ export class AddressButtonComponent implements OnDestroy {
 
   getLatLng(): void {
     this.subscription.add(
-      this.helperApiService.getLatLng(this.group.value.location__city, this.group.value.location__street).subscribe({
+      this.helperApiService
+        .getLatLng(this.group.value.location__city, this.group.value.location__street)
+        .subscribe({
           next: (response: any) => {
             // eslint-disable-next-line no-console
             console.log('response', response);
