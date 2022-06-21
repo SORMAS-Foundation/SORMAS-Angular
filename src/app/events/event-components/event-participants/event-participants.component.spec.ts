3005be5b23885439a86379319b2311b6438dce75
Sandor Ban <79192042+sandorbanvitagroup@users.noreply.github.com>
2022-06-21 14:37:13 +0300

    Case contact & event participant import (#482)
    * Case contact & event participant import
    
    * Fix Lint error
    
    * Fix failing test
    
    Co-authored-by: Sandor Ban <zorckmaster@gmail.com>

--- src/app/events/event-components/event-participants/event-participants.component.spec.ts
+++ src/app/events/event-components/event-participants/event-participants.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 import { EventParticipantsComponent } from './event-participants.component';
 
 describe('EventParticipantsComponent', () => {
@@ -12,7 +13,12 @@ describe('EventParticipantsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EventParticipantsComponent],
-      imports: [TranslateModule.forRoot(), HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatDialogModule,
+      ],
     }).compileComponents();
   });
 
--- src/app/events/event-components/event-participants/event-participants.component.ts
+++ src/app/events/event-components/event-participants/event-participants.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 import { Filter, NavItem, TableColumn } from '../../../_models/common';
 import { CONFIG_CASES } from '../../../_constants/storage';
 import { defaultColumnDefs } from './event-participants-list-table-data';
 import { EventParticipantService } from '../../../_services/api/event-participant.service';
 import { actionsBulkEditDefs } from './event-participants-list-actions-data';
-import { EVENT_PARTICIPANTS_FILTERS_FORM_ID } from '../../../app.constants';
+import {
+  ACTIONS_EVENT_PARTICIPANT,
+  ADD_MODAL_WIDE,
+  EVENT_PARTICIPANTS_FILTERS_FORM_ID,
+} from '../../../app.constants';
 import { FormBase } from '../../../shared/dynamic-form/types/form-element-base';
 import { FORM_DATA_EVENT_PARTICIPANTS_FILTERS } from '../event-participants-filters/event-participants-filters-form-data';
+import { ImportModalComponent } from '../../../shared/modals/import-modal/import-modal.component';
 
 @Component({
   selector: 'app-event-participants',
@@ -27,8 +33,21 @@ export class EventParticipantsComponent implements OnInit, OnDestroy {
 
   constructor(
     public eventParticipantService: EventParticipantService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private dialog: MatDialog
   ) {}
+
+  openImportModal(): void {
+    this.dialog.open(ImportModalComponent, {
+      width: ADD_MODAL_WIDE,
+      data: {
+        title: 'strings.headingImportEventParticipant',
+        type: ACTIONS_EVENT_PARTICIPANT.IMPORT,
+        service: this.eventParticipantService,
+        selectDate: true,
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.defaultColumns = defaultColumnDefs;
